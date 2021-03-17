565495ae7813c2a5823d5777404f288c5de5b43a
CoPham <copv1110@gmail.com>
2021-03-17 08:48:26 +0700

    Finalizing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import 'aos/dist/aos.css';
 
 function App() {
 	useEffect(() => {
-		Aos.init({ duration: 2000 });
+		Aos.init({ disable: 'mobile', duration: 2000 });
 	}, []);
 	return (
 		<div className="App">
